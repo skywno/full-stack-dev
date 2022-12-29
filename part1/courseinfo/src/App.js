07c6fcaa33f88a6c0d249d80dac1c0870f6bd57c
Ezra Lim <skywno@gmail.com>
2022-12-29 18:08:51 +0200

    Complete exercise 1.3 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,7 +9,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.section.part} {props.section.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -17,15 +17,16 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part section={props.sections[0]} />
-      <Part section={props.sections[1]} />
-      <Part section={props.sections[2]} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
 
 const Total = (props) => {
-  const sum = props.exercises.reduce((accumulator, currentValue) => accumulator + currentValue)
+  const sum = props.parts.map(part => part.exercises)
+    .reduce((accumulator, currentValue) => accumulator + currentValue)
   return (
     <p>
       Number of exercises {sum}
@@ -33,37 +34,29 @@ const Total = (props) => {
   )
 }
 
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const section1 = new Section(part1, exercises1)
-  const section2 = new Section(part2, exercises2)
-  const section3 = new Section(part3, exercises3)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    }, {
+      name: 'State of a component',
+      exercises: 14
+    }]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content sections={[section1, section2, section3]} />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
-
-class Section {
-  constructor(part, exercises) {
-    this.part = part
-    this.exercises = exercises
-  }
-}
-
 
 export default App
