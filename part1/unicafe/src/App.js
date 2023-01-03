659605f9d3e469deb2579517ba9ab19dad397bef
Ezra Lim <skywno@gmail.com>
2023-01-03 18:06:16 +0200

    Complete the exercise 1.11*

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,14 +12,34 @@ const Statistics = ({ good, bad, neutral }) => {
 
   if (sum > 0) {
     return (
-      <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={sum} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} suffix="%" />
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{sum}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{positive} %</td>
+          </tr>
+        </tbody>
+      </table>
     )
   } else {
     return (
