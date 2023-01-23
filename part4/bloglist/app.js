89f354e77f07e54ca583ebff1c0f23180c99937e
Ezra Lim <skywno@gmail.com>
2023-01-23 15:12:13 +0200

    complete 4.1-4.5 (skip 4.6 & 4.7)

---
+++ part4/bloglist/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const express = require('express')
+
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
